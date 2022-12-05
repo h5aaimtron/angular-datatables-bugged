6df343898558c25ff9f9e509cb677d3e29dc5e2d
Scott Ladd <sladd.33@gmail.com>
2022-12-05 11:02:11 -0800

    DataTablesError

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Without this import, cli works fine, with it, we get eeInjectorDeclaration errors
+import { DataTablesModule } from 'angular-datatables';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
